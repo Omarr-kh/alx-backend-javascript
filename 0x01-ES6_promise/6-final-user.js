3d75d8632819bc834706601d9aa156b9d3ba3b4e
omar <omar1752003@gmail.com>
2024-03-06 20:54:20 +0000

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,26 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) => {
+    const returnedArray = [];
+
+    for (const result of values) {
+      if (result.status === 'fulfilled') {
+        returnedArray.push({
+          status: result.status,
+          value: result.value,
+        });
+      } else {
+        returnedArray.push({
+          status: result.status,
+          value: result.reason,
+        });
+      }
+    }
+    return returnedArray;
+  });
+}
