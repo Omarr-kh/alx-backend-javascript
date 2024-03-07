52480799182fe84586e5bf4a29c93759a3bec896
omar <omar1752003@gmail.com>
2024-03-07 15:59:41 +0000

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new Error('Invalid input type for sqft');
+    }
+    this._sqft = sqft;
+
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function') {
+      throw Error(
+        'Class extending Building must ' + 'override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
