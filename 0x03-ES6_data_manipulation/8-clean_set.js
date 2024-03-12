2bb93cc87227377f357829239bbbe5d4f5552c8c
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-03-13 00:34:28 +0200

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,16 @@
 export default function cleanSet(set, startString) {
-  const parts = [];
+  const cleanedStr = [];
   if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
   for (const value of set.values()) {
     if (typeof value === 'string' && value.startsWith(startString)) {
-      const valueSubStr = value.substring(startString.length);
+      const str = value.substring(startString.length);
 
-      if (valueSubStr && valueSubStr !== value) {
-        parts.push(valueSubStr);
+      if (str && str !== value) {
+        cleanedStr.push(str);
       }
     }
   }
-  return parts.join('-');
+  return cleanedStr.join('-');
 }
