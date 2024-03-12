ad805829153af4e2fbc7c1bf4acd2c919ffb9c8a
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-03-12 21:52:10 +0200

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,5 +4,6 @@ export default function cleanSet(set, startString) {
   return [...set]
     .filter((item) => item.startsWith(startString))
     .map((item) => item.slice(startString.length))
+    .filter((item) => item !== '')
     .join('-');
 }
