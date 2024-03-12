5c75dadca86f4f640b1175564342388b7b776b14
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-03-12 17:01:39 +0200

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,14 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  // Use map to create a new array with modified students
   return students.map((student) => {
-    // Check if the student is in the specified city
-    if (student.location === city) {
-      // Find the matching grade for the current student
-      const matchingGrade = newGrades.find((newGrade) => newGrade.studentId === student.id);
+    // Create a new object to avoid modifying the original parameter
+    const updatedStudent = { ...student };
 
-      // Update the grade based on the matching grade or set it to 'N/A'
-      student.grade = matchingGrade ? matchingGrade.grade : 'N/A';
+    if (updatedStudent.location === city) {
+      const matchingGrade = newGrades.find((newGrade) => newGrade.studentId === updatedStudent.id);
+
+      updatedStudent.grade = matchingGrade ? matchingGrade.grade : 'N/A';
     }
 
-    // Return the modified or unmodified student
-    return student;
+    return updatedStudent;
   });
 }
