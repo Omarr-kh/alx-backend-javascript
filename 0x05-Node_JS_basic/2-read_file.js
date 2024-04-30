4360170f059a0a1ad15397d96340fe77b525414c
Omar <omar1752003@gmail.com>
2024-04-30 13:36:33 +0000

    eslint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -7,37 +7,38 @@ const fs = require('fs');
  */
 const countStudents = (path) => {
   try {
-    data = fs.readFileSync(path, 'utf8');
+    const data = fs.readFileSync(path, 'utf8');
+
+    const dataLines = data
+      .split('\n')
+      .filter((line) => line !== '')
+      .slice(1);
+    const columnValues = dataLines.map((line) => line.split(','));
+
+    console.log(`Number of students: ${columnValues.length}`);
+
+    const studentsData = {};
+
+    columnValues.forEach((column) => {
+      const firstName = column[0];
+      const field = column[3];
+
+      if (!studentsData[field]) {
+        studentsData[field] = [firstName];
+      } else {
+        studentsData[field].push(firstName);
+      }
+    });
+
+    for (const field in studentsData) {
+      if (Object.hasOwnProperty.call(studentsData, field)) {
+        const students = studentsData[field].join(', ');
+        const fieldCount = studentsData[field].length;
+        console.log(`Number of students in ${field}: ${fieldCount}. List: ${students}`);
+      }
+    }
   } catch (err) {
     throw new Error('Cannot load the database');
-  }
-  dataLines = data
-    .split('\n')
-    .filter((line) => line !== '')
-    .slice(1);
-  const columnValues = dataLines.map((line) => line.split(','));
-
-  console.log(`Number of students: ${columnValues.length}`);
-
-  const studentsData = {};
-
-  columnValues.forEach((column) => {
-    firstName = column[0];
-    field = column[3];
-
-    if (!studentsData[field]) {
-      studentsData[field] = [firstName];
-    } else {
-      studentsData[field].push(firstName);
-    }
-  });
-
-  for (const field in studentsData) {
-    students = studentsData[field].join(', ');
-    fieldCount = studentsData[field].length;
-    console.log(
-      `Number of students in ${field}: ${fieldCount}. List: ${students}`
-    );
   }
 };
 
