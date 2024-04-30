f02a2927d902b64b4928768b23f4c0ca83978a27
Omar <omar1752003@gmail.com>
2024-04-30 19:59:22 +0000

    following ling

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -3,9 +3,7 @@ import mapRoutes from './routes';
 
 const app = express();
 mapRoutes(app);
-app.listen(1245, () => {
-  console.log(`Server listening on port 1245`);
-});
+app.listen(1245);
 
 export default app;
 module.exports = app;
