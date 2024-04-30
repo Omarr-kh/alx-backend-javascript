5638db9a645bf218a4194bc2be9f80aacf7e5988
Omar <omar1752003@gmail.com>
2024-04-30 13:08:13 +0000

    reade csv file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  dataLines = data
+    .split('\n')
+    .filter((line) => line !== '')
+    .slice(1);
+  const columnValues = dataLines.map((line) => line.split(','));
+
+  console.log(`Number of students: ${columnValues.length}`);
+
+  const studentsData = {};
+
+  columnValues.forEach((column) => {
+    firstName = column[0];
+    field = column[3];
+
+    if (!studentsData[field]) {
+      studentsData[field] = [firstName];
+    } else {
+      studentsData[field].push(firstName);
+    }
+  });
+
+  for (const field in studentsData) {
+    students = studentsData[field].join(', ');
+    fieldCount = studentsData[field].length;
+    console.log(
+      `Number of students in ${field}: ${fieldCount}. List: ${students}`
+    );
+  }
+};
+
+module.exports = countStudents;
+
